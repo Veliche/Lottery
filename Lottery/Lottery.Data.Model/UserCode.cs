ebcaa0cd0f830150eff86c600eb4a36312437f42
Sajberton <tonimihajlovski@yahoo.co.uk>
2018-09-01 12:28:54 +0200

    cas 2

---
+++ Lottery/Lottery.Data.Model/UserCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery.Data.Model
+{
+    [Table("dbo.UserCodes")]
+
+    public class UserCode : IEntity
+    {
+        [Key]
+        [Column("UserCodeID", Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        [Column("CodeID")]
+        public int CodeID { get; set; }
+        public Code Code { get; set; }
+        public DateTime SendAt { get; set; }
+
+    }
+}
---
+++ Lottery/Lottery.Data.Model/UserCodeAward.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery.Data.Model
+{
+    [Table("dbo.UserCodeAwards")]
+
+    public class UserCodeAward : IEntity
+    {
+        [Key]
+        [Column("UserCodeAwardID", Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Column("UserCodeId")]
+        public int UserCodeId { get; set; }
+        public UserCode UserCode { get; set; }
+        [Column("AwardID")]
+        public int AwardID { get; set; }
+        public Award Award { get; set; }
+        public DateTime WonAt { get; set; }
+    }
+}
